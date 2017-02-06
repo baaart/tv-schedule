3a99861a852d6cbb8c7ed56cf46d96e4bb4bc735
Bartlomiej Wojcieszek <bartlomiej.wojcieszek@netguru.pl>
2017-02-06 13:32:59 +0100

    Add spec for schedule api

---
+++ spec/requests/api/v1/schedules_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Schedules API" do
+  it 'retrieves a list of schedules' do
+    FactoryGirl.create_list(:schedule, 10)
+
+    get '/api/v1/schedules'
+
+    json= JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json['schedules'].length).to eq(10)
+  end
+
+  it 'retrieves a single schedule' do
+    schedule = FactoryGirl.create(:schedule)
+
+    get "/api/v1/schedules/#{schedule.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json['schedule']['channel']['id']).to eq(schedule.channel.id)
+    expect(json['schedule']['created_at']).to eq(nil)
+  end
+end
