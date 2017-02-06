f94409c4f49184091c4177642e164aaaf4131673
Bartlomiej Wojcieszek <bartlomiej.wojcieszek@netguru.pl>
2017-02-06 14:44:58 +0100

    Fix rubocop violations

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :schedule do
     show
     channel
-    start_time "2017-02-06 11:35:37"
-    end_time "2017-02-06 11:35:37"
+    start_time '2017-02-06 11:35:37'
+    end_time '2017-02-06 11:35:37'
   end
 end
