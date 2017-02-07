5e18839e89a0a5636bbffb50681786204d8b0199
Bartlomiej Wojcieszek <bartlomiej.wojcieszek@netguru.pl>
2017-02-07 23:06:44 +0100

    Remove unnecesary routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get '/schedules/search', to: 'schedules#search'
-      resources :schedules
-      resources :shows
-      resources :channels
-      resources :categories
+      resources :schedules, only: [:show, :index]
+      resources :shows, only: [:show, :index]
+      resources :channels, only: [:show, :index]
+      resources :categories, only: [:show, :index]
     end
   end
 
