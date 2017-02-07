bb5859648a2a624bfe4c1674bf400237b5c523c7
Bartlomiej Wojcieszek <bartlomiej.wojcieszek@netguru.pl>
2017-02-07 10:44:49 +0100

    Add scheduler to demo page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,61 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require moment
+//= require fullcalendar.min.js
+//= require scheduler
+//= require demo
 //= require_tree .
+
+
+genres = { 'comedy':'green', 'horror':'red', 'series':'blue',
+          'sci-fi':'brown', 'romance':'violet', 'news':'orange' }
+
+$( document ).ready(function() {
+  $('#calendar').fullCalendar({
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    now: '2017-02-07',
+    aspectRatio: 1.8,
+    scrollTime: '00:00', // undo default 6am scrollTime
+    header: {
+      left: 'today prev,next',
+      center: 'title',
+      right: 'timelineDay,timelineThreeDays,agendaWeek,month'
+    },
+    defaultView: 'timelineDay',
+    timeFormat: 'H(:mm)',
+    views: {
+      timelineThreeDays: {
+        type: 'timeline',
+        duration: { days: 3 }
+      }
+    },
+    resourceLabelText: 'Channels',
+    resources: '/api/v1/channels',
+    events: function(start,end,timezone,callback)
+        {
+            $.ajax({
+              url: '/api/v1/schedules',
+              dataType: "json",
+              success: function(data) {
+                var events = [];
+                $.each(data, function(index, schedule) {
+                    events.push({
+                      id: schedule.id,
+                      title: schedule.show.title,
+                      start: schedule.start_time,
+                      end: schedule.end_time,
+                      resourceId: schedule.channel.id,
+                      color: genres[schedule.show.category.name]
+                    });
+                });
+                callback(events);
+              },
+              error: function (request, status, error) {
+                alert(request.responseText);
+                callback([]);
+              }
+            });
+        }
+  });
+});
